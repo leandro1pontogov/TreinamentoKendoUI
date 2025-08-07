9f7fd6c0cb67d8d37f17e794fd2e7a7b584f94d3
unknown <leandro@pontogovsistemas.com.br>
2025-08-07 08:56:18 -0300

    feat: Desenvolvimento botao incluir e editar funcionais

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
 $(function () {
-   $("#grid").kendoGrid({
-  columns: [
-    { field: "Nome" },
-    { field: "Categoria" },
-	{ field: "Preco" },
-	{ field: "DataCadastro" },
-	{ field: "Ativo" },
-  ],
-  columnMenu: true,
-  dataSource: [
-    { Nome: "01", Categoria: "002", Preco: "Em Andamento",  DataCadastro: "Teste", Ativo: "Sim"},
-    { Id: "02", Sequencia: 33 }
-  ]
-});
-
-$("#toolbar").kendoToolBar({
-        items: [
-            { type: "button", id: "btn1", text: "Incluir", click:function(){
-			var $incluir = $("#incluir");
-
-			if (!$incluir.data("kendoWindow")) {
-        	$incluir.kendoWindow({
-            visible: false,
-            width: 500,
-            height: 500
-        });
-    }
-		var incluir = $("#incluir").data("kendoWindow");
-		incluir.open().center();
-
-			}},
-            { type: "button", id: "btn2", text: "Editar", click:function(){
-			var $editar = $("#editar");
-
-			if (!$editar.data("kendoWindow")) {
-        	$editar.kendoWindow({
-            visible: false,
-            width: 500,
-            height: 500
-        });
-    }
-		var editar = $("#editar").data("kendoWindow");
-		editar.open().center();
-
-			} }
-        ],
-    });
+  $("#grid").kendoGrid({
+    columns: [
+      { field: "Nome" },
+      { field: "Categoria" },
+      { field: "Preco" },
+      { field: "DataCadastro" },
+      { field: "Ativo" },
+    ],
+    columnMenu: true,
+    dataSource: [
+      { Nome: "Celular", Categoria: "Eletronico", Preco: "2000.0", DataCadastro: "07/08/2025", Ativo: "Sim" },
+      { Nome: "Sofa", Categoria: "Moveis", Preco: "750", DataCadastro: "02/03/2025", Ativo: "Nao" },
+    ]
   });
+
+  $("#toolbar").kendoToolBar({
+    items: [
+      {
+        type: "buttonGroup",
+        buttons: [
+          {
+            text: "Incluir", click: function () {
+
+              if (!$("#tela-cadastro").data("kendoWindow")) {
+                $("#tela-cadastro").kendoWindow({
+                  width: 500,
+                  height: 300,
+                  title: "Cadastro",
+                  visible: false
+                });
+              }
+
+
+              if (!$("#textbox").data("kendoTextBox")) {
+                $("#textbox").kendoTextBox({
+                  label: "Nome",
+                  placeholder: "Digite o nome..."
+                });
+              }
+
+              $("#textbox").closest(".k-textbox").show();
+
+              $("#tela-cadastro").data("kendoWindow").center().open();
+            }
+          },
+          {
+            text: "Editar", click: function () {
+              if (!$("#tela-cadastro").data("kendoWindow")) {
+                $("#tela-cadastro").kendoWindow({
+                  visible: false,
+                  width: 500,
+                  height: 500,
+                  title: "Editar"
+                });
+              }
+
+              if (!$("#textbox").data("kendoTextBox")) {
+                $("#textbox").kendoTextBox({
+                  label: "Nome",
+                  placeholder: "Digite o nome..."
+                });
+              }
+
+              $("#textbox").closest(".k-textbox").show();
+
+              $("#tela-cadastro").data("kendoWindow").center().open();
+            }
+          },
+        ]
+      },
+      {
+      }
+    ]
+  });
+});
